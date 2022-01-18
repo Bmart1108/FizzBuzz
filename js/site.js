43e5b44ba06313325c229db1e84cfa725731f21c
Bmart1108 <bmart1108@gmail.com>
2022-01-18 17:24:29 -0500

    Changed template names, added a new table for fizzbuzz

--- js/site.js
+++ js/site.js
@@ -1,14 +1,86 @@
 // display a message to the user
-function getMessage() {
+function getValues() {
+    alert("Hello App");
+}
 
-    let userMessage = document.getElementById("message").value;
+function getValues2() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
 
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Your work has been saved',
-        showConfirmButton: false,
-        timer: 1500
-    })
-    console.log(userMessage);
-};
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+
+    //check to see if they are in fact integers
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+
+        //generate a list of numbers
+        let numbers = generateNumbers(startValue, endValue)
+
+
+
+        //display a list of numbers
+        displayNumbers(numbers);
+
+
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Only integers are allowed for FizzBuzz"
+        })
+    }
+}
+
+
+function generateNumbers(start, end, ) {
+
+    let numbers = [];
+    for (let index = start; index <= end; index++) {
+        numbers.push(index);
+    }
+
+    return numbers;
+
+
+
+    /*
+   pos=  0,1,2,3,4,5,6,7,8 
+        [1,2,3,4,5,6,7,8,9]
+*/
+
+}
+
+
+function displayNumbers(numbers) {
+    let templateRows = "";
+
+    for (let index = 1; index < numbers.length; index++) {
+
+        let number = numbers[index];
+
+        let result = "";
+
+        if (number % 15 == 0) {
+            result = "fizzbuzz";
+
+        } else if (number % 5 == 0) {
+            result = "buzz";
+
+        } else if (number % 3 == 0) {
+            result = "fizz"
+
+        } else {
+            result = number;
+        }
+
+        let row = `<tr><td class="${result}">${result}</td></tr>`;
+
+        templateRows += row;
+
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+
+}
